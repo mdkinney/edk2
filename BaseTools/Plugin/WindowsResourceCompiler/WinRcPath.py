ac0a50f7aa9430a968bb0ed5d920539207e8a9f8
Michael D Kinney <michael.d.kinney@intel.com>
2025-12-11 12:09:59 -0800

    BaseTools/Plugin/WindowsResourceCompiler: Disable
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- BaseTools/Plugin/WindowsResourceCompiler/WinRcPath.py
+++ BaseTools/Plugin/WindowsResourceCompiler/WinRcPath.py
@@ -27,6 +27,8 @@ class WinRcPath(IUefiBuildPlugin):
                     self._set_path(rc_path)
                     return 0
 
+        return 0
+
         # If it does not exist, try to find it with FindToolInWinSdk
         path = locate_tools.FindToolInWinSdk("rc.exe")
         if path is None:
